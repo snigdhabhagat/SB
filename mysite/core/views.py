07b2ebf4250dcab0e984508bd3277fbf186064d9
akuma515 <ajay.t.kumar@capgemini.com>
2018-10-15 12:11:32 +0530

    added webserver configs

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
 
 @login_required
 def home(request):
@@ -22,3 +22,31 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def slide4(request):
+    return render(request, "slide 4.html", {})  # 'form': form})
+
+
+def slide5(request):
+    return render(request, "slide 5.html", {})
+
+
+def slide7(request):
+    return render(request, "slide 7.html", {})
+
+
+def slide8(request):
+    return render(request, "slide 8.html", {})
+
+
+def slide9(request):
+    return render(request, "slide 9.html", {})
+
+
+def slide10(request):
+    return render(request, "slide 10.html", {})
+
+
+def slide11(request):
+    return render(request, "slide 11.html", {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
-
+from django.urls import re_path
 from mysite.core import views as core_views
 
 
@@ -9,4 +9,12 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
+    re_path(r'^slide4/$', core_views.slide4, name='slide4'),
+    re_path(r'^slide5/$', core_views.slide5, name='slide5'),
+    re_path(r'^slide7/$', core_views.slide7, name='slide7'),
+    re_path(r'^slide8/$', core_views.slide8, name='slide8'),
+    re_path(r'^slide9/$', core_views.slide9, name='slide9'),
+    re_path(r'^slide10/$', core_views.slide10, name='slide10'),
+    re_path(r'^slide11/$', core_views.slide11, name='slide11'),
+
 ]
